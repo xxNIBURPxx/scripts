b755e28e5032d5f025f3b86d4b0aacd6fc735c36
xxNIBURPxx <121443417+xxNIBURPxx@users.noreply.github.com>
2022-12-27 10:17:19 -0800

    netsh

---
+++ General/netsh-domainbind-cmds.ps1
@@ -0,0 +1,12 @@
+# Set DNS servers cmd
+powershell -Command “Start-Process cmd -Verb RunAs”
+
+# Run netsh
+netsh
+interface ip set dns name="Ethernet" source="static" address="10.180.44.90"
+
+# Join domain via CMD prompt
+netdom /domain:niburp.local /user:phil-da /password:addyourown member niburp-pc01 /joindomain
+
+# Join domain via PowerShell
+Add-Computer -DomainName niburp.local
