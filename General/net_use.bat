f1369410f59e6b002ca4390058cd6179a7940d16
xxNIBURPxx <121443417+xxNIBURPxx@users.noreply.github.com>
2022-12-30 10:58:10 -0800

    net use

---
+++ General/net_use.bat
@@ -0,0 +1,5 @@
+net use m: \\niburp-syn01\media /user:plexy bR58w*NMkJ!Fbq$JV9y6 /persistent:yes
+net use p: \\niburp-syn01\data /user:plexy bR58w*NMkJ!Fbq$JV9y6 /persistent:yes
+net use s: \\niburp-syn01\software /user:plexy bR58w*NMkJ!Fbq$JV9y6 /persistent:yes
+net use z: \\niburp-syn01\usbshare1-2 /user:plexy bR58w*NMkJ!Fbq$JV9y6 /persistent:yes
+net use x: \\niburp-syn01\usbshare2 /user:plexy bR58w*NMkJ!Fbq$JV9y6 /persistent:yes
