e6ac78dc747863f2a4a058322c08a80cb74d5ce7
xxNIBURPxx <121443417+xxNIBURPxx@users.noreply.github.com>
2023-01-07 11:42:09 -0800

    net_use

---
+++ General/net_use.bat
@@ -0,0 +1,5 @@
+net use m: \\niburp-syn01\media /persistent:yes
+net use p: \\niburp-syn01\data /persistent:yes
+net use s: \\niburp-syn01\software /persistent:yes
+net use z: \\niburp-syn01\usbshare1-2 /persistent:yes
+net use x: \\niburp-syn01\usbshare2 /persistent:yes
