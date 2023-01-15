986527ca674446cc58b2baab847f902a9d0363e5
xxNIBURPxx <121443417+xxNIBURPxx@users.noreply.github.com>
2023-01-15 05:33:25 -0800

    AD

--- AD/Set-ADComputer.ps1
+++ AD/Set-ADComputer.ps1
@@ -2,4 +2,15 @@
 Get-ADComputer CABC056PS96M3
 
 #Set ADComputer object in AD
-Set-ADComputer CABC021DV1SN2 -Description "FOCV: Ruzickova, Alzbeta"
+Set-ADComputer Set-ADComputer CAAB14SIM14WSA -Description "SimphonyKiosk: CAAB14 F&F"
+Set-ADComputer CAAB14SIM14WSB -Description "SimphonyKiosk: CAAB14 F&F"
+Set-ADComputer CAAB14SIM14WSC -Description "SimphonyKiosk: CAAB14 F&F"
+Set-ADComputer CAAB14SIM14WSD -Description "SimphonyKiosk: CAAB14 F&F"
+Set-ADComputer CAAB14SIM14WSE -Description "SimphonyKiosk: CAAB14 F&F"
+Set-ADComputer CAAB14SIM14WSF -Description "SimphonyKiosk: CAAB14 F&F"
+Set-ADComputer USAK11SIM11WSB -Description "SimphonyKiosk: USAK11 TAL"
+Set-ADComputer USMT08-SIM08WSA -Description "SimphonyKiosk: USMT08 GML"
+Set-ADComputer USMT08-SIM08WSB -Description "SimphonyKiosk: USMT08 GML"
+Set-ADComputer USMT08-SIM08WSC -Description "SimphonyKiosk: USMT08 GML"
+Set-ADComputer USMT08-SIM08WSD -Description "SimphonyKiosk: USMT08 GML"
+Set-ADComputer USMT08-SIM08WSF -Description "SimphonyKiosk: USMT08 GML"
