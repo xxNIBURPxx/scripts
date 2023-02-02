fd86c2c90a75120294e3a058c416959d2bd53b30
xxNIBURPxx <121443417+xxNIBURPxx@users.noreply.github.com>
2023-02-02 05:30:35 -0800

    MSOLUser

--- Azure/MSOLUser.ps1
+++ Azure/MSOLUser.ps1
@@ -11,3 +11,5 @@ Get-MsolUser -UserPrincipalName "ychan@pursuitcollection.com"
 #set user upn
 Set-MsolUserPrincipalName -UserPrincipalName "ychan@viadcorp.onmicrosoft.com" -NewUserPrincipalName "ychan@pursuitcollection.com"
 
+#  Check for errors
+Get-MsolUser -HasErrorsOnly | fl DisplayName,UserPrincipalName,@{Name="Error";Expression={($_.errors[0].ErrorDetail.objecterrors.errorrecord.ErrorDescription)}} > MSOL-Errors.txt
